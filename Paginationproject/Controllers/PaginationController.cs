7c64a941b59752ef6b8a58106e90cc6bbcf5fa6e
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-04 17:19:41 +0530

    Add files via upload

---
+++ Paginationproject/Controllers/PaginationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Paginationproject.Models;
+
+namespace Paginationproject.Controllers
+{
+    public class PaginationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Display()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\727837\source\repos\Paginationproject\Paginationproject\App_Data\Empdatabase.mdf;Integrated Security=True");
+            con.Open();
+            string sql = "SELECT * FROM PaginationTable";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            List<PaginationModel> emp1 = new List<PaginationModel>();
+            PaginationModel details;
+
+            using (SqlDataReader read = cmd.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    details = new PaginationModel();
+                    details.EmployeeId = int.Parse(read["EmployeeId"].ToString());
+                    details.FirstName = (read["FirstName"].ToString());
+                    details.LastName = read["LastName"].ToString();
+                    details.EmailId = read["EmailId"].ToString();
+                    details.ProjectId = int.Parse(read["ProjectId"].ToString());
+                    details.ProjectName = read["ProjectName"].ToString();
+                    emp1.Add(details);
+
+                }
+            }
+            return Json(emp1);
+        }
+    }
+}
---
+++ Paginationproject/Models/PaginationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Paginationproject.Models
+{
+    public class PaginationModel
+    {
+
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+
+    }
+}
