fa65231b0dcf47a4775145918294fe17585a5b5d
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-22 15:48:00 +0530

    Update PaginationController.cs

--- Paginationproject/Controllers/PaginationController.cs
+++ Paginationproject/Controllers/PaginationController.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.IO;
 using System.Web.Mvc;
 using System.Data.SqlClient;
 using Paginationproject.Models;
+using System;
+using System.Configuration;
 
 namespace Paginationproject.Controllers
 {
@@ -15,33 +13,42 @@ namespace Paginationproject.Controllers
         {
             return View();
         }
-        [HttpPost]
-        public JsonResult Display()
+        
+        [HttpGet]
+        public ActionResult Display()
         {
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\727837\source\repos\Paginationproject\Paginationproject\App_Data\Empdatabase.mdf;Integrated Security=True");
-            con.Open();
-            string sql = "SELECT * FROM PaginationTable";
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.ExecuteNonQuery();
             List<PaginationModel> employee = new List<PaginationModel>();
-            PaginationModel details;
-
-            using (SqlDataReader read = cmd.ExecuteReader())
+            try
             {
-                while (read.Read())
-                {
-                    details = new PaginationModel();
-                    details.EmployeeId = int.Parse(read["EmployeeId"].ToString());
-                    details.FirstName = (read["FirstName"].ToString());
-                    details.LastName = read["LastName"].ToString();
-                    details.EmailId = read["EmailId"].ToString();
-                    details.ProjectId = int.Parse(read["ProjectId"].ToString());
-                    details.ProjectName = read["ProjectName"].ToString();
-                    employee.Add(details);
+                DBModel data = new DBModel();
+                SqlConnection connection = data.DBconnection();
+                connection.Open();
+                string sql = "SELECT * FROM PaginationTable";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+                PaginationModel details;
 
+                using (SqlDataReader read = command.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+                        details = new PaginationModel();
+                        details.EmployeeId = int.Parse(read["EmployeeId"].ToString());
+                        details.FirstName = (read["FirstName"].ToString());
+                        details.LastName = read["LastName"].ToString();
+                        details.EmailId = read["EmailId"].ToString();
+                        details.ProjectId = int.Parse(read["ProjectId"].ToString());
+                        details.ProjectName = read["ProjectName"].ToString();
+                        employee.Add(details);
+
+                    }
                 }
             }
-            return Json(employee);
+            catch (Exception e)
+            {
+
+            }
+            return Json(employee,JsonRequestBehavior.AllowGet);
         }
     }
 }
