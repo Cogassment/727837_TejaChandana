90633980fb3499990911aebd5600a7bbac6c431c
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-22 15:45:02 +0530

    Add files via upload

---
+++ Paginationproject/Models/DBModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Paginationproject.Models
+{
+    public class DBModel
+    {
+        public SqlConnection DBconnection()
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Constring"].ConnectionString);
+            return connection;
+        }
+    }
+    
+}
